0db0da3c5f98c09272d712f8ca017683cea95385
BuildTools <unconfigured@null.spigotmc.org>
2023-07-08 05:31:23 +0800

    added params.py

---
+++ params.py
@@ -0,0 +1,10 @@
+file_loc = {'sensor1_file': 'data_engineer_test/sensor_1.xlsx', 
+            'sensor2_file': 'data_engineer_test/sensor_2.csv',
+            'sensor4_file': 'data_engineer_test/sensor_4.parquet',
+            'sensor4_dd_file': 'data_engineer_test/sensor_4_diff_date.pickle',
+            'sensor5_file': 'data_engineer_test/sensor_5.json'}
+
+sensor_info = {'Sensor 1': 'sens_1',
+               'Sensor 2': 'sens_2',
+               'Sensor 4': 'sens_4',
+               'Sensor 5': 'sens_5',}
